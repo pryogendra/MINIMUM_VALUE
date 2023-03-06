7e0dabc60b9c14771ef684fbe918f99cb6f11bc4
Yogendra Prasad <127005415+pryogendra@users.noreply.github.com>
2023-03-06 09:40:49 +0530

    Add files via upload

---
+++ min.php
@@ -0,0 +1,7 @@
+<html>
+    <?php
+    print(min(123,234,65,457,122,23))."<br>";  //min() returns the minimum value 
+    print(min(1,34,5,7,12,2))."<br>";
+    print(min(13,24,6,57,12,23))."<br>";
+    ?>
+</html>
